06fd68d6f4fedee053f227a51275b80f24395f3b
LuisAntonimo <luisantoniossouza@yahoo.com.br>
2020-05-17 16:46:48 -0300

    main function applied

---
+++ index.js
@@ -0,0 +1,34 @@
+const Twit = require('twit');
+
+require('dotenv').config();
+const config = require('./config');
+
+const bot = new Twit(config);
+
+const stream = bot.stream('statuses/filter', {
+  track: ['@KnucklesReview'], //check mentions
+});
+
+stream.on('tweet', tweetEvent); //trigger event
+
+function tweetEvent(tweet) {
+  const filePath = './src/appvd.mp4';
+
+  bot.postMediaChunked({ file_path: filePath }, function (err, data, response) {
+    const idString = data.media_id_string;
+    const name = tweet.user.screen_name;
+    const replyID = tweet.id_str;
+
+    const params = {
+      status: `@${name}`,
+      media_ids: [idString],
+      in_reply_to_status_id: replyID,
+    };
+
+    bot.post('statuses/update', params, function (err, data, response) {
+      console.log(data);
+    });
+  });
+}
+
+console.log('bot running... ✅');
