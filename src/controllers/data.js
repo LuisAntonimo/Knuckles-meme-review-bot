841382222a203002c7c93db3e5e554d67eb39569
LuisAntonimo <luisantoniossouza@yahoo.com.br>
2020-05-21 19:12:40 -0300

    new features

--- src/controllers/data.js
+++ src/controllers/data.js
@@ -1,4 +1,16 @@
-module.exports = {
-  code: 'approved',
-  source: './public/appvd.mp4',
-};
+data = [
+  {
+    code: '@KnucklesReview approved',
+    source: './public/appvd.mp4',
+  },
+  {
+    code: '@KnucklesReview denied',
+    source: './public/deny.mp4',
+  },
+  // {
+  //   code: '@KnucklesReview dunno',
+  //   source: './public/idk.mp4',
+  // },
+];
+
+module.exports = data;
--- src/index.js
+++ src/index.js
@@ -4,35 +4,47 @@ require('dotenv').config();
 const config = require('./controllers/config'); //api access
 const videos = require('./controllers/data'); //video database
 
-const bot = new Twit(config);
+for (let video in videos) {
+  const bot = new Twit(config);
 
-const stream = bot.stream('statuses/filter', {
-  track: ['@KnucklesReview'], //check mentions
-});
-
-stream.on('tweet', tweetEvent); //trigger event
-
-function tweetEvent(tweet) {
-  const filePath = videos.source;
-
-  //chunked media upload function
-  bot.postMediaChunked({ file_path: filePath }, function (err, data, response) {
-    const idString = data.media_id_string;
-    const name = tweet.user.screen_name;
-    const replyID = tweet.id_str;
-
-    //reply params
-    const params = {
-      status: `@${name}`,
-      media_ids: [idString], //chunked video id
-      in_reply_to_status_id: replyID,
-    };
-
-    //reply
-    bot.post('statuses/update', params, function (err, data, response) {
-      console.log(data);
-    });
+  const stream = bot.stream('statuses/filter', {
+    track: ['@KnucklesReview'], //check mentions
   });
+
+  stream.on('tweet', tweetEvent); //trigger event
+
+  function tweetEvent(tweet) {
+    const txt = tweet.text;
+    console.log(txt);
+
+    if (txt === videos[video].code) {
+      const filePath = videos[video].source;
+
+      // chunked media upload function
+      bot.postMediaChunked({ file_path: filePath }, (err, data, response) => {
+        const idString = data.media_id_string;
+
+        const name = tweet.user.screen_name;
+        const replyID = tweet.id_str;
+
+        //reply params
+        const params = {
+          status: `@${name}`,
+          media_ids: [idString], //chunked video id
+          in_reply_to_status_id: replyID,
+        };
+
+        //reply
+        bot.post('statuses/update', params, function (err, data, response) {
+          if (err !== undefined) {
+            console.log(err);
+          } else {
+            console.log(`Replied ${name}`);
+          }
+        });
+      });
+    }
+  }
 }
 
 console.log('✅ bot running... ');
